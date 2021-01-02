d2eb7cf0b94b854a3484e9896545a1dd86701a22
Danil Sheglov <68465904+sheglovofficial@users.noreply.github.com>
2021-01-02 21:58:07 +0300

    Code changes

--- mainApp/Program.cs
+++ mainApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace mainApp
 {
@@ -7,25 +9,31 @@ namespace mainApp
         static void Main(string[] args)
         {
             string name = "";
+            string path = @"C:\Programming\Console-App\mainApp\info.txt"; // File creation path (you can specify your own)
             int age = 0, height = 0, weight = 0;
 
             // Global variables are declared
 
-            Console.Write("Enter your name: ");
-            name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\t\t\t==============================================");
+            Console.Write("\n\t\t\t\t\tEnter your name: ");
+            name = Console.ReadLine();      // Username input
 
             bool ageCorrect = false;
             while (ageCorrect == false)
             {
                 try
                 {
-                    Console.Write("Enter your age: ");
-                    age = Int32.Parse(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\t\t\t\t\tEnter your age: ");
+                    age = Int32.Parse(Console.ReadLine());     // Age input
                     ageCorrect = true;
                 }
                 catch
                 {
-                    Console.WriteLine("Please use Integer, not String!");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t\t\t\t\tERROR: Please use Integer, not String!");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
             }
 
@@ -34,13 +42,16 @@ namespace mainApp
             {
                 try
                 {
-                    Console.Write("Enter your height: ");
-                    height = Int32.Parse(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\t\t\t\t\tEnter your height: ");
+                    height = Int32.Parse(Console.ReadLine());       // Height input
                     heightCorrect = true;
                 }
                 catch
                 {
-                    Console.WriteLine("Please use Integer, not String!");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t\t\t\t\tERROR: Please use Integer, not String!");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
             }
 
@@ -49,34 +60,63 @@ namespace mainApp
             {
                 try
                 {
-                    Console.Write("Enter your weight: ");
-                    weight = Int32.Parse(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\t\t\t\t\tEnter your weight: ");
+                    weight = Int32.Parse(Console.ReadLine());       // Weight input
                     weightCorrect = true;
                 }
                 catch
                 {
-                    Console.WriteLine("Please use Integer, not String!");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t\t\t\t\tERROR: Please use Integer, not String!");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
             }
 
-            Console.Write("Enter your place of residence: ");
-            string residenceAddress = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\t\t\t\t\tEnter your place of residence: ");
+            string residenceAddress = Console.ReadLine();       // Residence Address input
+
+            Console.WriteLine("\n\t\t\t\t==============================================");
 
             Console.WriteLine();
 
-            // All data is assigned to its own variables. Ready to withdraw 
 
-            Console.WriteLine("========= INFORMATION ABOUT YOU =========");
+            Console.WriteLine("\t\t\t\t\tFile recording starts!\n");
 
-            Console.WriteLine($"1. Name: {name}");
-            Console.WriteLine($"2. Age: {age}");
-            Console.WriteLine($"3. Height: {height}");
-            Console.WriteLine($"4. Weight: {weight}");
-            Console.WriteLine($"5. Residence Address: {residenceAddress}");
+            for (int i = 0; i < 1; i++)
+            {
+                Console.Write("\t\t\t\t\tLoading [######");
+                Thread.Sleep(1000);
+                Console.Write("###");
+                Thread.Sleep(1000);
+                Console.Write("####");
+                Thread.Sleep(1000);
+                Console.Write("######");
+                Thread.Sleep(1000);
+                Console.WriteLine("#######] - 100%\n");
+            }
 
-            Console.WriteLine("=========================================");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\t\t\t\t\tRecording completed!\n");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            Console.WriteLine("\t\t\t\t\tPress the Enter key to close the program");
 
             Console.ReadKey();
+
+            string[] infoUser = new string[] { "========= INFORMATION ABOUT YOU =========\n",
+                                            $"\t1. Name: {name}",
+                                            $"\t2. Age: {age}",
+                                            $"\t3. Height: {height}",
+                                            $"\t4. Weight: {weight}",
+                                            $"\t5. Residence Address: {residenceAddress}\n",
+                                            "========================================="};
+
+            File.WriteAllLines(path, infoUser);
         }
     }
 }
